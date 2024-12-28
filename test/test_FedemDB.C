e7b98901316fd8591e11fc234c430b760d717c62
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2024-12-28 18:55:24 +0100

    Add some unit tests for FedemDB without using fedempy

---
+++ test/test_FedemDB.C
@@ -0,0 +1,105 @@
+// SPDX-FileCopyrightText: 2023 SAP SE
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// This file is part of FEDEM - https://openfedem.org
+////////////////////////////////////////////////////////////////////////////////
+
+/*!
+  \file test_FedemDB.C
+  \brief Unit testing for FedemDB.
+*/
+
+#include "gtest.h"
+
+extern "C" {
+  void FmInit(const char* = NULL, const char* = NULL);
+  void FmNew(const char* = NULL);
+  bool FmSave(const char* = NULL);
+  void FmClose(bool = true);
+  int  FmCreateTriad(const char*, double, double, double,
+                     double = 0.0, double = 0.0, double = 0.0, int = 0);
+  bool FmAddMass(int, int, const double*, int = 0);
+  int  FmCreatePart(const char*, int, int*);
+  int  FmCreateJoint(const char*, int, int, int*, int);
+}
+
+static std::string srcdir; //!< Full path of the source directory of this test
+
+
+/*!
+  \brief Main program for the unit test executable.
+*/
+
+int main (int argc, char** argv)
+{
+  // Initialize the google test module.
+  // This will remove the gtest-specific values in argv.
+  ::testing::InitGoogleTest(&argc,argv);
+
+  // Extract the source directory of the tests
+  // to use as prefix for loading external files
+  for (int i = 1; i < argc; i++)
+    if (!strncmp(argv[i],"--srcdir=",9))
+    {
+      srcdir = argv[i]+9;
+      std::cout <<"Note: Source directory = "<< srcdir << std::endl;
+      if (srcdir.back() != '/') srcdir += '/';
+      break;
+    }
+
+  // Initialize the Fedem mechanism database
+  FmInit();
+
+  // Invoke the google test driver
+  int status = RUN_ALL_TESTS();
+
+  // Clean up heap memory
+  FmClose();
+
+  return status;
+}
+
+
+/*!
+  \brief Unit test creating the simplest solvable model.
+*/
+
+TEST(TestFedemDB,Simplest)
+{
+  FmNew("simplest.fmm");
+
+  double mass[4] = { 5.0, 1.1, 1.2, 1.3 };
+  ASSERT_TRUE(FmAddMass(FmCreateTriad("T1",0.0,0.0,0.0),4,mass));
+  ASSERT_TRUE(FmSave());
+}
+
+
+/*!
+  \brief Unit test creating two prismatic joints close to each other.
+*/
+
+TEST(TestFedemDB,Prismatic)
+{
+  const int PRISMATIC = 11;
+
+  FmNew("prismatic.fmm");
+
+  std::vector<int> triads;
+  char T[3] = "T0";
+  char M[3] = "M0";
+  double x = 0.0;
+  for (int i = 0; i < 10; i++, x += 10.0, T[1]++)
+    triads.push_back(FmCreateTriad(T, x, 0.0, 0.0));
+  x = 0.0;
+  for (int i = 0; i < 8; i++, x += 11.25, M[1]++)
+    triads.push_back(FmCreateTriad(M, x, 0.25, 0.3));
+  int s1 = FmCreateTriad("S1", 0.5, 0.0, 0.0);
+  int s2 = FmCreateTriad("S2", 0.5, 0.25, 0.3);
+  int m1[2] = { triads.front(), triads[9] };
+  int m2[2] = { triads[10], triads.back() };
+  ASSERT_GT(FmCreatePart("My part", triads.size(), triads.data()), 0);
+  ASSERT_GT(FmCreateJoint("J1", PRISMATIC, s1, m1, 2), 0);
+  ASSERT_GT(FmCreateJoint("J2", PRISMATIC, s2, m2, 2), 0);
+  ASSERT_TRUE(FmSave());
+}
